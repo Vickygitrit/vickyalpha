8315efb3e57202278dcdab449a822f5038f3d09d
Vicky Kumar <vickykmr96312@gmail.com>
2023-12-21 05:04:17 +0530

    alpha

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import SessionProvider from './SessionProvider';
+
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full bg-gray-900">
+      <head>
+        <title>Giphy</title>
+      </head>
+      <body className="h-full">
+      <SessionProvider>
+        {children}
+      </SessionProvider>
+      </body>
+    </html>
+  )
+}
